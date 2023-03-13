a7e0bbe4631e592414aa38d797cb26e113959d36
mintamzxje <am237doc@gmail.com>
2023-03-13 14:04:17 +0700

    mintamzxje

---
+++ src/main/java/com/sso/SSO_BE_DG2_8/SsoBeDg28Application.java
@@ -0,0 +1,13 @@
+package com.sso.SSO_BE_DG2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoBeDg28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoBeDg28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sso/SSO_BE_DG2_8/SsoBeDg28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.SSO_BE_DG2_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoBeDg28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
