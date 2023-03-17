54717cdc87e0cc8e7017191b76989004068683b8
Duy Phước Phạm <107868539+phuocxl@users.noreply.github.com>
2023-03-17 17:07:50 +0700

    Update ResponseDTO.java

--- src/main/java/com/sso/payload/response/ResponseDTO.java
+++ src/main/java/com/sso/payload/response/ResponseDTO.java
@@ -1,1 +1,17 @@
+package com.sso.payload.response;
 
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ResponseDTO {
+    private boolean success;
+    private HttpStatus code;
+    private String errorMessage;
+    private Object data;
+
+}
