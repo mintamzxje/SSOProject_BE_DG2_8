77722ecfd6b09213e75381aaf56702e892e20d06
lam <nguyentruonglamhs@gmail.com>
2023-04-04 15:27:46 +0700

    add document for api import/export excel

--- src/main/java/com/sso/controller/UtilsController.java
+++ src/main/java/com/sso/controller/UtilsController.java
@@ -2,6 +2,8 @@ package com.sso.controller;
 
 import com.sso.model.User;
 import com.sso.service.UserExcelService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,7 @@ public class UtilsController {
     @Autowired
     UserExcelService userExcelService;
 
+    @ApiOperation(value = "this api export a file excel which contain all users", notes = "Return a file excel users ")
     @GetMapping("/export")
     public void exportUsers(HttpServletResponse response){
         response.setContentType("application/octet-stream");
@@ -31,6 +34,7 @@ public class UtilsController {
         }
     }
 
+    @ApiOperation(value = "this api import all users from file excel to component", notes = "Save users ")
     @PostMapping("/import/{componentId}")
     public List<User> importUsers(@RequestPart MultipartFile file, @PathVariable String componentId){
         List<User> result = null;
--- src/main/java/com/sso/payload/dto/UserDTO.java
+++ src/main/java/com/sso/payload/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.sso.payload.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Data
@@ -9,9 +10,12 @@ import lombok.*;
 @Getter
 @Setter
 public class UserDTO {
+    @Schema(title = "uuid",required = true)
     private String uuid;
     private String userName;
+    @Schema(title = "email",required = true, pattern = "/^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$/")
     private String email;
+    @Schema(example = "124124")
     private String password;
     private String fullName;
     private String firstName;
@@ -19,5 +23,4 @@ public class UserDTO {
     private String phone;
     private String address;
     private String avatar;
-
 }
